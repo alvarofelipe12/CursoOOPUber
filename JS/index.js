e5b5f4642f01eda6eb828c76894532af30a2e8e3
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2021-10-10 09:27:07 -0500

    Classes heritage for JS and Python
    - Add new child classes

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Herrera", "DOC123"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new Car("QEW456", new Account("Andrea Fiti", "JOJO123"), "Chevy", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
