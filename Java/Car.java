e3b160540127bbc96f65a712db182f253c8ec434
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2021-10-10 10:55:44 -0500

    Encapsulating Car class
    - Add validation for passengers to be 4.

--- Java/Car.java
+++ Java/Car.java
@@ -4,7 +4,7 @@ public class Car {
   Integer id;
   String license;
   Account driver;
-  Integer passenger;
+  private Integer passenger;
 
   public Car(String license, Account driver) {
     this.license = license;
@@ -12,6 +12,20 @@ public class Car {
   }
 
   void printDataCar() {
-    System.out.println("License: " + license + " Driver Name: " + driver.name);
+    if (passenger != null) {
+      System.out.println("License: " + license + " Driver Name: " + driver.name);
+    }
+  }
+
+  public Integer getPassenger() {
+    return passenger;
+  }
+
+  public void setPassenger(Integer passenger) {
+    if (passenger == 4) {
+      this.passenger = passenger;
+    } else {
+      System.out.println("Need to assign 4 passengers");
+    }
   }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -8,17 +8,17 @@ import java.time.YearMonth;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
-    Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123", "email@email.com", "123"));
-    car.passenger = 4;
-    car.printDataCar();
+    UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123", "email@email.com", "123"), "Chevrolet", "Spark");
+    uberX.setPassenger(4);
+    uberX.printDataCar();
 
-    Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876", "email@email.com", "123"));
-    car2.passenger = 3;
-    System.out.println("Car2 License " + car2.license);
-    car2.printDataCar();
+    // Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876", "email@email.com", "123"));
+    // car2.passenger = 3;
+    // System.out.println("Car2 License " + car2.license);
+    // car2.printDataCar();
 
-    Card card = new Card(123456, "415412341234", "123", YearMonth.of(2022, 6));
-    System.out.println("Card number " + card.number);
+    // Card card = new Card(123456, "415412341234", "123", YearMonth.of(2022, 6));
+    // System.out.println("Card number " + card.number);
   }
   
 }
